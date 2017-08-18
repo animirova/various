c5132f679f4dbfb4c26a69065c36620e760be80c
animirova <ani.mirova@gmail.com>
2017-08-18 01:39:23 -0400

    more stuff is working

--- stuff.js
+++ stuff.js
@@ -1,4 +1,33 @@
 /**
  * Created by ani on 8/17/17.
  */
-$('#loginRegisterModal').modal('show');
+(function () {
+    // $('#loginRegisterModal').modal('show');
+
+    var tagPopoverContent = "<div><a href='google.com'>#google</a>, " +
+        "<a href='facebook.com'>#facebook</a>, <a href='blizzard.com'>blizzard</a>, " +
+        "<a href='blizzard.com'>#overwatch</a></div>";
+
+    $(document).ready(function(){
+        $('#tags-badge').popover({
+            placement: 'left',
+            animation: true,
+            html: true,
+            content:tagPopoverContent
+        });
+    });
+
+
+    // $(function () {
+    //     $('[data-toggle="popover"]').popover()
+    // });
+    // $(function () {
+    //     ('[data-toggle="tooltip"]').tooltip();
+    // });
+    // $('#tags-badge').tooltip({
+    //     html: true,
+    //     title: "hello",
+    //     content: $('#popover-content').html(),
+    //     placement: 'left'
+    // });
+})();
